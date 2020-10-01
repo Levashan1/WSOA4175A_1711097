9113051a5861850ca19fd2212e5dd3e16cb0e887
DESKTOP-Q3NT8VK\GodlyZhan <Zhanshilee11@gmail.com>
2020-10-01 08:52:43 +0200

    new layout

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,44 @@
-const menuData = [
+console.log("working");
+const blogPosts = [
+  {title: "wireframe", date: "thedate"}
+  
+];
+
+const menuItem = [
 
   { title: 'Home', link: '/index'},
-  { title: 'Blog', link: '/Blog'},
+  { title: 'Blog', link: '/Blog', children: blogPosts},
   { title: 'About', link: '/About'},
+];
 
-]
-const intializemenu = () => {
-    const Nav = document.querySelector('nav');
-    
-    const Ul = document.createElement('ul');
-    
-    for(let item of ['Home', 'Blog', 'About']){
-        const Li = document.createElement('li');
-        Li.innerText = item;
+const createMenuItem = (item) => {
+  const Li = document.createElement('li');
+  const a = document.createElement('a');
+  a.innerText = item.title;
+  a.href = item.link;
+  Li.appendChild(a);
+
+
+        if(item.children && item.children.lenght){
+
+          let ul = document.createElement('ul');
+          for(let childitem of item.children){
+            let childMenuitem = createMenuItem(childitem);
+            ul.appendChild(childMenuitem);
+          }
+          li.appendChild(ul);
+        }
         
-        document.Ul.appendChild(Li);
-    
+return Li;
+};
+const intializemenu = () => {
+    const nav = document.querySelector('nav');
+    const ul = document.createElement('ul');
+   
+    for(let item of menuItem){
+       const li = createMenuItem(item);
+       ul.appendChild(Li);
     }
-    document.Nav.appendChild(ul);
+    document.nav.appendChild(ul);
     };
 document.addEventListener('DOMContentLoaded', () => intializemenu());
