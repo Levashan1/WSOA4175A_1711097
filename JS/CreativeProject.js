2a36b26b0565a4c4e4840ef48dede82d270ed7da
DESKTOP-Q3NT8VK\GodlyZhan <Zhanshilee11@gmail.com>
2020-10-23 14:03:55 +0200

    new webpage
    changed everything

--- JS/CreativeProject.js
+++ JS/CreativeProject.js
@@ -1,10 +1,20 @@
-/*const games = document.querySelector('#Projects-subbody');
 
-let gameslist = document.createElement("ol");
+var mybutton = document.getElementById("myBtn");
 
-const button = document.querySelector('button');
 
-button.onclick = function(){
-    let name = prompt('what is your name?');
-    alert('Hello '+ name +',nice to see you!');
-}*/
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click",
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
